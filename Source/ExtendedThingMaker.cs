d60e000917712b5dd9d196d09f63c01d325edc62
flabbet <flubbet@gmail.com>
2019-04-30 21:34:52 +0200

    Mod upload

---
+++ Source/ExtendedThingMaker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Emergency_Resources
+{
+    public static class ExtendedThingMaker
+    {
+        public static Thing MakeThingWithAmount(ThingDef def, int stackCount)
+        {
+            Thing thing = ThingMaker.MakeThing(def);
+            thing.stackCount = stackCount;
+            return thing;
+        }
+    }
+}
---
+++ Source/VerbEmergencyRadio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace Emergency_Resources
+{
+    public class VerbEmergencyRadio : Verb
+    {
+        private readonly List<Thing> _emergencyResourcesList = new List<Thing>()
+        {
+            ExtendedThingMaker.MakeThingWithAmount(ThingDefOf.Steel, 300),
+            ExtendedThingMaker.MakeThingWithAmount(ThingDefOf.WoodLog, 500),
+            ExtendedThingMaker.MakeThingWithAmount(ThingDefOf.MealSurvivalPack, 50),
+            ExtendedThingMaker.MakeThingWithAmount(ThingDefOf.ComponentIndustrial, 50),
+            ExtendedThingMaker.MakeThingWithAmount(ThingDefOf.MedicineIndustrial, 50),
+        };
+
+        protected override bool TryCastShot()
+        {
+            if (currentTarget.HasThing && currentTarget.Thing.Map != caster.Map)
+                return false;
+            Map target = caster.Map;
+            IntVec3 intVec3 = currentTarget.Cell;
+            DropPodUtility.DropThingsNear(intVec3, target, _emergencyResourcesList, 110, false, true);
+            if (EquipmentSource != null && !EquipmentSource.Destroyed)
+                EquipmentSource.Destroy();
+            return true;
+        }
+    }
+}
